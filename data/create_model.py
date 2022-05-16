deb6309e6e9a262d0f4a9d06678f6ba249474cf6
Kiran Varma <datlakiran147@gmail.com>
2022-05-16 09:52:36 +0530

    Add files via upload

---
+++ data/create_model.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from joblib import dump
+from datetime import datetime
+from sklearn.linear_model import LinearRegression
+
+src_file = 'ipl.csv'
+dest_file = 'model.joblib'
+
+df = pd.read_csv(src_file)
+
+columns_to_remove = ['mid', 'venue', 'batsman', 'bowler', 'striker', 'non-striker']
+
+df.drop(labels=columns_to_remove, axis=1, inplace=True)
+
+consistent_teams = ['Kolkata Knight Riders', 'Chennai Super Kings', 'Rajasthan Royals',
+                    'Mumbai Indians', 'Kings XI Punjab', 'Royal Challengers Bangalore',
+                    'Delhi Daredevils', 'Sunrisers Hyderabad']
+
+df = df[(df['bat_team'].isin(consistent_teams)) & (df['bowl_team'].isin(consistent_teams))]
+
+df = df[df['overs']>=5.0]
+
+
+df['date'] = df['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+
+encoded_df = pd.get_dummies(data=df, columns=['bat_team', 'bowl_team'])
+
+encoded_df = encoded_df[['date', 'bat_team_Chennai Super Kings', 'bat_team_Delhi Daredevils', 'bat_team_Kings XI Punjab',
+              'bat_team_Kolkata Knight Riders', 'bat_team_Mumbai Indians', 'bat_team_Rajasthan Royals',
+              'bat_team_Royal Challengers Bangalore', 'bat_team_Sunrisers Hyderabad',
+              'bowl_team_Chennai Super Kings', 'bowl_team_Delhi Daredevils', 'bowl_team_Kings XI Punjab',
+              'bowl_team_Kolkata Knight Riders', 'bowl_team_Mumbai Indians', 'bowl_team_Rajasthan Royals',
+              'bowl_team_Royal Challengers Bangalore', 'bowl_team_Sunrisers Hyderabad',
+              'overs', 'runs', 'wickets', 'runs_last_5', 'wickets_last_5', 'total']]
+
+X_train = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year <= 2016]
+X_test = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year >= 2017]
+
+y_train = encoded_df[encoded_df['date'].dt.year <= 2016]['total'].values
+y_test = encoded_df[encoded_df['date'].dt.year >= 2017]['total'].values
+
+X_train.drop(labels='date', axis=True, inplace=True)
+X_test.drop(labels='date', axis=True, inplace=True)
+
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+dump(regressor, dest_file)
